b04102de670b9eaa9b016b224ce767c99c9e8b78
Maya <foenx0315@gmail.com>
2023-06-28 18:55:51 +0530

    f

--- next.config.js
+++ next.config.js
@@ -2,31 +2,28 @@
 module.exports = {
     reactStrictMode: true,
     async redirects() {
-      return [
-        {
-          source: "/github",
-          destination: "https://github.coom/TopicBotList/Ruby-Hoshino",
-          permanent: false,
-        },
-      ];
-      module.exports = withPWA({
-        swcMinify: true,
-        compress: true,
-        optimizeFonts: true,
-        images: {
-            formats: ["image/avif", "image/webp"],
-            dangerouslyAllowSVG: true,
-            remotePatterns: [
-                {
-                    protocol: "https",
-                    hostname: "cdn.topiclist.xyz",
-                },
-                {
-                    protocol: "https",
-                    hostname: "api.producthunt.com",
-                },
-            ],
-    },
-  });
- }
+        return [{
+            source: "/github",
+            destination: "https://github.com/TopicBotList/Ruby-Hoshino",
+            permanent: false,
+        }, ];
+        module.exports = withPWA({
+            swcMinify: true,
+            compress: true,
+            optimizeFonts: true,
+            images: {
+                formats: ["image/avif", "image/webp"],
+                dangerouslyAllowSVG: true,
+                remotePatterns: [{
+                        protocol: "https",
+                        hostname: "cdn.topiclist.xyz",
+                    },
+                    {
+                        protocol: "https",
+                        hostname: "api.producthunt.com",
+                    },
+                ],
+            },
+        });
+    }
 }
