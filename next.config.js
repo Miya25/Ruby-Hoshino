29e8329f045fc239dfd782a9b09aa1f3b57a7105
Maya <foenx0315@gmail.com>
2023-06-30 20:18:22 +0530

    stuff
    supports mobile now LOL

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+    dest: "public",
+    disable: process.env.NODE_ENV == "development",
+});
 module.exports = {
     reactStrictMode: true,
     async redirects() {
@@ -24,6 +28,8 @@ module.exports = {
                     },
                 ],
             },
+            deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+            imageSizes: [16, 32, 48, 64, 96, 100, 128, 150, 256, 384],
         });
 
     }
